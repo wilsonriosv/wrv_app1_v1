1250dba5df167c0cf8aca4b923218b2b4670cb45
wilsonriosv <wilsonriosv@gmail.com>
2024-11-27 17:47:29 -0500

    Corregido el problema de navegación entre los tabs. Continuamos con el diseño del contenido de cada tab

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,12 @@ export default function RootLayout() {
   import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
   import Icon from "react-native-vector-icons/MaterialIcons";
   
+  // Importa las pantallas que corresponden a cada tab
+import HomeScreen from "./home/index";
+import RecordsScreen from "./records/index";
+import WeatherScreen from "./weather/index";
+import SettingsScreen from "./settings/index";
+
   const Tab = createBottomTabNavigator();
   
   export default function Layout() {
@@ -17,30 +23,30 @@ export default function RootLayout() {
                   tabBarIcon: ({ color, size }) => {
                       let iconName;
                       switch (route.name) {
-                          case "home":
+                          case "Home":
                               iconName = "home";
                               break;
-                          case "records":
+                          case "Records":
                               iconName = "list";
                               break;
-                          case "weather":
+                          case "Weather":
                               iconName = "cloud";
                               break;
-                          case "settings":
+                          case "Settings":
                               iconName = "settings";
                               break;
                           default:
-                              iconName = "home"; // Valor predeterminado si no se encuentra coincidencia
+                              iconName = "Home"; // Valor predeterminado si no se encuentra coincidencia
                               break;
                       }
                       return <Icon name={iconName} size={size} color={color} />;
                   },
               })}
           >
-              <Tab.Screen name="home" component={Slot} />
-              <Tab.Screen name="records" component={Slot} />
-              <Tab.Screen name="weather" component={Slot} />
-              <Tab.Screen name="settings" component={Slot} />
+              <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Records" component={RecordsScreen} />
+            <Tab.Screen name="Weather" component={WeatherScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
           </Tab.Navigator>
       );
   }
